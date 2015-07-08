163f2f29e045e96a823ce4ccf07702d287821eeb
vsaxena115 <vsaxena115@gmail.com>
2015-07-08 12:36:39 +0530

    new scripts

--- ContosoWeb.ps1/ContosoWebsite.ps1
+++
@@ -1,5 +0,0 @@
-Configuration ContosoWebsite
-{
-  Powershell.exe -File C:\Program Files\WindowsPowerShell\Modules\scripts\createMof.ps1
-  Powershell.exe -File C:\Program Files\WindowsPowerShell\Modules\scripts\setPull.ps1
-} 
---
+++ ContosoWebsite.ps1
@@ -0,0 +1,5 @@
+Configuration ContosoWebsite
+{
+  Powershell.exe -File 'C:\Program Files\WindowsPowerShell\Modules\scripts\createMof.ps1'
+  Powershell.exe -File 'C:\Program Files\WindowsPowerShell\Modules\scripts\setPull.ps1'
+} 
