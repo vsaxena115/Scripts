870c4bbcbf17025d4b7aee85f34a779fa7cdbe04
vsaxena115 <vsaxena115@gmail.com>
2015-07-08 11:47:32 +0530

    new change

---
+++ ContosoWeb.ps1/scripts/TestConfig.ps1
@@ -0,0 +1,5 @@
+configuration TestConfig
+{
+Powershell.exe -File C:\Program Files\WindowsPowerShell\Modules\createMof.ps1
+Powershell.exe -File C:\Program Files\WindowsPowerShell\Modules\setPull.ps1
+}TestConfig
---
+++ ContosoWeb.ps1/scripts/createMof.ps1
@@ -0,0 +1,37 @@
+Configuration TestConfig {
+
+    Param(
+        [Parameter(Mandatory=$True)]
+        [String[]]$NodeGUID
+    )
+    
+    Node $NodeGUID {
+
+       WindowsFeature IIS  
+        {  
+            Ensure          = "Present"  
+            Name            = "Web-Server"  
+        } 
+
+    }
+}
+
+$Computers = @("build","dev","test")
+
+write-host "Generating GUIDs and creating MOF files..."
+foreach ($Node in $Computers)
+    {
+    $NewGUID = [guid]::NewGuid()
+    $NewLine = "{0},{1}" -f $Node,$NewGUID
+    TestConfig -NodeGUID $NewGUID
+    $NewLine | add-content -path "$env:SystemDrive\Program Files\WindowsPowershell\DscService\Configuration\dscnodes.csv"
+    }
+
+write-host "Creating checksums..."
+New-DSCCheckSum -ConfigurationPath .\TestConfig -OutPath .\TestConfig -Verbose -Force
+
+write-host "Copying configurations to pull service configuration store..."
+$SourceFiles = (Get-Location -PSProvider FileSystem).Path + "\TestConfig\*.mof*"
+$TargetFiles = "$env:SystemDrive\Program Files\WindowsPowershell\DscService\Configuration"
+Move-Item $SourceFiles $TargetFiles -Force
+Remove-Item ((Get-Location -PSProvider FileSystem).Path + "\TestConfig\")
